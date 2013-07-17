c08800a899986499cd768476a2412c05f54f3813
Michael Moore <stuporglue@gmail.com>
2013-07-17 17:32:58 -0500

    Individual links on the tree page

--- tree.php
+++ tree.php
@@ -17,7 +17,30 @@
             <?php require_once('lib/header.php'); ?>
             <h2>A TreeTrumpet Demo</h2>
             <p>
-                This is a bowtie view of part of my family tree. Other possible views include ancestors only and descendants only. 
+                <?php
+                    require_once(__DIR__ . '/lib/3rdparty/ged2json.php');
+                    $ged2json = new ged2json(__DIR__ . '/family.ged');
+                    $ancestors = $ged2json->toJsonArray(TRUE);
+                    usort($ancestors,function($a,$b){
+                        return str_replace('/','',$a['name']) > str_replace('/','',$b['name']);
+                    });
+                    $ids = Array();
+                    foreach($ancestors as $ancestor){
+                        $ids[$ancestor['id']] = $ancestor['name'];
+                    }
+                    ksort($ids);
+
+                    foreach($ids as $id => $name){
+                        print "<p>Back to the home person: <a href='individual.php?id={$id}' onclick=\"return refocusTree('{$id}');\">" . preg_replace('|/(.*)/|',"<span class='ttln'>$1</span>",$name) . "</a></p>";
+                        break;
+                    }
+
+                    print "<ul>";
+                    foreach($ancestors as $ancestor){
+                        print "<li><a href='individual.php?id={$ancestor['id']}' onclick=\"return refocusTree('{$ancestor['id']}');\">" . preg_replace('|/(.*)/|',"<span class='ttln'>$1</span>",$ancestor['name']) . "</a></li>";
+                    }
+                    print "</ul>";
+                ?>
             </p>
         </div>
 
@@ -46,8 +69,6 @@
         <script type="text/javascript" src="js/3rdparty/make_chart.js"></script>
         <script type="text/javascript" src="js/3rdparty/tree.js"></script>
 
-
-
         <!-- Kick it off once everything is ready -->
         <script type="text/javascript"> 
             $(document).ready(function(){
@@ -58,6 +79,16 @@
                     }
                 });
             });
+
+            // refocus the tree on someone and move the page so that the tree is in view
+            // return false so that the link isn't followed. The link is there for 
+            // javascriptless users and brings them to the individual page
+            function refocusTree(id){
+                pt.refocus(id);
+                window.location.hash='tree';
+                window.location=window.location;
+                return false;
+            }
         </script> 	
 </body>
 </html>
