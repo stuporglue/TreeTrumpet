ca4807805a0c91311193fd963c7382a41ecc592d
Michael Moore <stuporglue@gmail.com>
2013-07-17 00:10:27 -0500

    build.php WIP, updated PV

--- build.php
+++ build.php
@@ -5,24 +5,47 @@
 # It is meant to be run from the command line, but should work from a browser 
 # if the server has write permissions to the current folder
 
-chdir(__DIR__);
-
 $destdir = 'treetrumpet';
+
+$exclude_from_zip = Array(
+    './lib/3rdparty/ssgeocoder.sqlite3',
+    './family.ged',
+    './robots.txt',
+    './config.php'
+);
+
+chdir(__DIR__);
+umask(0); 
+
+function recurse_copy($src,$dst) { 
+    $dir = opendir($src); 
+    @mkdir($dst,'0777',TRUE); 
+    while(false !== ( $file = readdir($dir)) ) { 
+        if (( $file != '.' ) && ( $file != '..' )) { 
+            if ( is_dir($src . '/' . $file) ) { 
+                recurse_copy($src . '/' . $file,$dst . '/' . $file); 
+            } 
+            else { 
+                copy($src . '/' . $file,$dst . '/' . $file); 
+            } 
+        } 
+    } 
+    closedir($dir); 
+} 
 
 
 // Create directories and set version info
 {
     $directories = Array(
-        "$destdir",
         "$destdir/css/3rdparty/ui",
-        "$destdir/img/3rdparty",
         "$destdir/js/3rdparty/ui",
-        "$destdir/lib/3rdparty/php-gedcom/library/",
+        "$destdir/img/3rdparty",
+        "$destdir/lib/3rdparty",
         "$destdir/lib/licenses"
     );
 
     foreach($directories as $dir){
-        @mkdir($dir,'0755',TRUE);
+        mkdir($dir,'0777',TRUE);
         if(!is_dir($dir)){
             print "ERROR: Couldn't create $dir\n";
             exit();
@@ -33,6 +56,7 @@ $destdir = 'treetrumpet';
     $revision = "TreeTrumpet build from " . date('Y-m-d_H:M') . "\n";
     $gitinfo = explode("\n",`git rev-list HEAD`);
     $revision .= array_shift($gitinfo);
+    file_put_contents("$destdir/lib/version.txt",$revision);
 }
 
 
@@ -76,7 +100,7 @@ $destdir = 'treetrumpet';
                         copy("js/Leaflet.markercluster/dist/MarkerCluster.css", "$destdir/css/3rdparty/MarkerCluster.css");
                         copy("js/Leaflet.markercluster/dist/MarkerCluster.Default.css","$destdir/css/3rdparty/MarkerCluster.Default.css");
                         copy("js/Leaflet.markercluster/dist/MarkerCluster.Default.ie.css","$destdir/css/3rdparty/MarkerCluster.Default.ie.css");
-                        copy("js/Leaflet.markercluster/MIT-LICENSE.TXT","$destdir/lib/licenses/Leaflet.markercluster.txt");
+                        copy("js/Leaflet.markercluster/MIT-LICENCE.txt","$destdir/lib/licenses/Leaflet.markercluster.txt");
                     }
 
 // ged2json
@@ -88,7 +112,8 @@ $destdir = 'treetrumpet';
                             }
                         }
                         copy("lib/ged2json/examples/php/lib/ssgeocoder/ssgeocoder.php","$destdir/lib/3rdparty/ssgeocoder.php");
-                        copy("lib/ged2json/examples/php/lib/php-gedcom/library","$destdir/lib/3rdparty/php-gedcom/",TRUE);
+                        recurse_copy("lib/ged2json/examples/php/lib/php-gedcom/library","$destdir/lib/3rdparty/php-gedcom/library");
+
                     }
 
 // Pedigree Viewer 
@@ -114,9 +139,30 @@ $destdir = 'treetrumpet';
 // New zip file!
 {
     @unlink("$destdir.zip");
-    chmod($destdir,0755);
-    // make zip file
+    chmod($destdir,0777);
+    chdir($destdir);
 
+    $directories = Array('.');
+
+    $zip = new ZipArchive();
+    if($zip->open("../$destdir.zip",ZipArchive::CREATE) !== TRUE){
+        exit("Cannot open $destdir.zip for writing!");
+    }
+
+    while(count($directories) > 0){
+        $cur_dir = array_shift($directories);
+        foreach(glob("$cur_dir/*") as $file_or_dir){
+            if(is_dir($file_or_dir)){
+                $directories[] = $file_or_dir;
+            }else if(is_file($file_or_dir) && !in_array($file_or_dir,$exclude_from_zip)){
+                $zip->addFile($file_or_dir);
+            }
+        }
+    }
+
+    $zip->close();
+
+    chdir('..');
     $md5 = md5("$destdir.zip");
     file_put_contents("$destdir.zip.md5",$md5);
 }
