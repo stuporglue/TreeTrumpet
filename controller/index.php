abcf17d3cbb24e1e456e8e09cb101a093dad1e20
Michael Moore <stuporglue@gmail.com>
2013-08-22 08:18:25 -0500

    Work on making landing page nicer

--- controller/index.php
+++ controller/index.php
@@ -18,26 +18,54 @@ $page->title("The Genealogy of " . $focus->firstName());
 $page->h1("The Genealogy of " . $focus->firstBold() . " and " . ($totalPeople - 1) . " of " . $focus->posessive() . " relatives");
 
 $page->body .= "<h2>Explore the family of " . $focus->firstBold() . "</h2>";
+$page->body .= "<div>
+    This site has 
+    </div>"
 
-$page->body .= view('feature_preview',Array(
-    'title' => 'Tree View',
+$page->bodyright .= view('feature_preview',Array(
+    'title' => 'Pedigree Tree',
     'text' => 'Pan, zoom and click around this interactive tree view of the family tree.',
-    'link' => 'tree.php',
+    'link' => linky('tree.php'),
     'img' => 'img/tree.png'
 ),true);
 
-$page->body .= view('feature_preview',Array(
-    'title' => 'Map View',
+$page->bodyright .= view('feature_preview',Array(
+    'title' => 'Ancestor Events Map',
     'text' => 'See where ancestors important events occurred on this map.',
-    'link' => 'map.php',
+    'link' => linky('map.php'),
     'img' => 'img/map_preview.png'
 ),true);
 
-$page->body .= view('feature_preview',Array(
-    'title' => 'Table View',
+$page->bodyright .= view('feature_preview',Array(
+    'title' => 'People',
     'text' => 'Drill down to the ancestor you\'re looking for with the filter and sorting tools on this interactive table.',
-    'link' => 'table.php',
+    'link' => linky('table.php'),
     'img' => 'img/table.png'
 ),true);
 
-view('page',Array('page' => $page,'menu' => 'index'));
+
+$csses = Array(
+    "http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/css/jquery.dataTables.css",
+    "css/index.css",
+);
+foreach($csses as $css){
+    $page->css($css);
+}
+
+
+$scripts = Array(
+    "http://code.jquery.com/jquery-1.9.1.js",
+    "http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/jquery.dataTables.js",
+    "js/people.js"
+);
+foreach($scripts as $script){
+    $page->js($script);
+}
+$page->js("$(document).ready(function(){
+    tt = $('#tt-people').ttTable('lib/ged2json.php','family.ged');
+});",TRUE);
+
+
+
+
+view('page_v_split',Array('page' => $page,'menu' => 'index'));
--- view/feature_preview.php
+++ view/feature_preview.php
@@ -1,7 +1,7 @@
-<div class='tt-preview'>
 <h3><?php print $title;?></h3>
+<div class='tt-preview'>
 <p><?php print $text;?></p>
 <a href="<?php print $link;?>" title="<?php print $title;?>">
-<img src="<?php print $img;?>" alt="<?php print $title;?>"/>
+<img class='stretchy' src="<?php print $img;?>" alt="<?php print $title;?>"/>
 </a>
 </div>
