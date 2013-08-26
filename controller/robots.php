1d4b66c9fbb222a5fc3406c6b5e42ff614336478
Michael Moore <stuporglue@gmail.com>
2013-08-26 17:44:43 -0500

    Deny by default to subdirs, robots works

--- controller/robots.php
+++ controller/robots.php
@@ -18,5 +18,7 @@ $sitemap .= $_SERVER['SERVER_NAME'];
 $sitemap .= dirname($_SERVER['SCRIPT_NAME']);
 $sitemap .= '/lib/sitemap.php';
 
-view('robots');
+global $_BASEURL;
+view('robots',Array('sitemap' => $_BASEURL . '/sitemap.php'),TRUE);
+file_put_contents(__DIR__ . '/../robots.txt');
 exit();
--- view/robots.php
+++ view/robots.php
@@ -3,10 +3,16 @@ header("Content-type: text/plain;charset=utf-8");
 ?>
 # This robots.txt is generated on demand. You can rep
 User-agent: *
+Disallow: /cache
+Disallow: /controller
+Disallow: /css
+Disallow: /img
+Disallow: /js
 Disallow: /lib
-Disallow: setup.php
-
-<? 
+Disallow: /model
+Disallow: /view
+Disallow: /
+<?php 
 if(isset($sitemap)){
     print "Sitemap: $sitemap\n"; 
 }
