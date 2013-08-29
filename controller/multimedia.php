595b60990d5392386c397c83aaa3a7172add446c
Michael Moore <stuporglue@gmail.com>
2013-08-28 21:28:10 -0500

    Photos working. Other multimedia not yet tested

--- controller/multimedia.php
+++ controller/multimedia.php
@@ -1,19 +1,16 @@
 <?php
 
 function multimedia($id){
+    if(!array_key_exists('h',$_GET)){
+        $_GET['h'] = NULL;
+    }
+    if(!array_key_exists('w',$_GET)){
+        $_GET['w'] = NULL;
+    }
+    if(!array_key_exists('a',$_GET)){
+        $_GET['a'] = FALSE;
+    }
 
-    $gedcom = model('ttgedcom',Array(__DIR__ . '/../family.ged'));
-
-    $args = func_get_args();
-    $id = array_shift($args);
-
-    $obje = $gedcom->getObject($id);
-
-    $mime = $obje->mime();
-    print_r($mime);
-
-    $name = $obje->attachmentName();
-    print_r($name);
-
-
+    $file = join('/',func_get_args());
+    return obje::readfile($file,$_GET['h'],$_GET['w'],$_GET['a']);
 }
--- model/obje.php
+++ model/obje.php
@@ -4,6 +4,9 @@ class obje {
     var $obje;
     var $pretty_gedcom;
 
+    const THUMB_WIDTH = 250;
+    const THUMB_HEIGHT = 250;
+
     function __construct($obje,$gedcom){
         $this->pretty_gedcom = model('pretty_gedcom',Array($gedcom));
 
@@ -13,12 +16,10 @@ class obje {
         $this->obje = $obje;
     }
 
-    function mime(){
-        $mediafile = $this->webPath();
-        $fsfile = __DIR__ . '/../' . $mediafile;
+    function mime($file = NULL){
+        $fsfile = obje::fsPath($file);
 
         // File type
-        // $mime = `file -bi $file  | sed 's/;.*//'`;
         $finfo = finfo_open(FILEINFO_MIME_TYPE);
         if(file_exists($fsfile) && $mimeType = finfo_file($finfo, $fsfile)){
             return $mimeType;
@@ -29,14 +30,8 @@ class obje {
     function embedHtml(){
             $ret = "";
 
-            $file = $this->obje->getFile();
-            $mediafile = $this->webPath();
-            $fsfile = __DIR__ . '/../' . $mediafile;
-
             // File type
             // $mime = `file -bi $file  | sed 's/;.*//'`;
-            $finfo = finfo_open(FILEINFO_MIME_TYPE);
-            if(file_exists($fsfile) && $mimeType = finfo_file($finfo, $fsfile)){
             if($mimeType = $this->mime()){
 
                 $browserImages = Array('image/jpeg','image/gif','image/png');
@@ -45,7 +40,7 @@ class obje {
 
                 $ret .= $this->link();
                 if(in_array($mimeType,$browserImages)){
-                    $ret .= $this->halfLink() . "<img alt='".$this->title()."' src='".$this->thumbnail()."'/></a>";
+                    $ret .= $this->halfLink() . "<img class='ttthumbnail' width='".obje::THUMB_WIDTH."px' alt='".$this->title()."' src='".$this->thumbnail()."'/></a>";
                 }else if(in_array($mimeType,$browserAudio)){
                     $ret .= "<audio controls><source src='$file' type='$mimeType'>Your browser does not support the HTML5 audio element.</audio>";
                 }else if(in_array($mimeType,$browserVideos)){
@@ -81,7 +76,7 @@ class obje {
 
         // link to nowhere
         if(!$file){
-            return '#'; 
+            return FALSE; 
         }
 
         // Link to link
@@ -95,7 +90,7 @@ class obje {
             return $_BASEURL . '/' . $file;
         }
 
-        return '#';
+        return FALSE;
     }
 
     function link(){
@@ -128,28 +123,127 @@ class obje {
     }
 
     function thumbnail(){
-        return $this->href();
+        $file = $this->webPath();
+
+        $qs = Array(
+            'h' => obje::THUMB_HEIGHT,
+            'w' => obje::THUMB_WIDTH
+        );
+
+        $file .= "?" . http_build_query($qs);
+
+        return $file;
     }
 
-    function webPath(){
+    function webPath($file = NULL){
+        global $_BASEURL;
+
+        $file = obje::relPath($file);
+        $file = linky($_BASEURL . "/multimedia.php/$file");
+
+        return $file;
+    }
+
+    function relPath($file = NULL){
         global $_CONFIG;
 
-        if(!$_CONFIG['media_dir']){
-            return;
+        if(is_null($file)){
+            $file = $this->obje->getFile();
         }
 
-        $file = $this->obje->getFile();
+        if(!$_CONFIG['media_dir']){
+            return $file;
+        }
 
         if(strpos($file,$_CONFIG['media_dir']) === 0){
             $file = str_replace($_CONFIG['media_dir'],'',$file);
         }
 
-        return "/media/$file";
+        return $file;
+    }
+
+    function fsPath($file = NULL){
+        $file = obje::relPath($file);
+        return __DIR__ . '/../media/' . $file;
     }
 
     function attachmentName(){
-        return $this->obje->getId() . '_' . basename($this->webPath());
+        return basename($this->webPath());
     }
+
+    function readfile($basefile = NULL,$h = NULL,$w = NULL,$attachment = FALSE){
+        $mime = obje::mime($basefile);
+        $file = obje::fsPath($basefile);
+        $attachmentName = basename($file);
+
+        if(!file_exists($file)){
+            return controller('_404');
+        }
+
+        if(!is_null($h) || !is_null($w)){
+            switch($mime){
+                case 'image/jpeg':
+                    $img = imagecreatefromjpeg($file);
+                    $outfunc = 'imagejpeg';
+                    break;
+                case 'image/png':
+                    $img = imagecreatefrompng($file);
+                    $outfunc = 'imagepng';
+                    break;
+                case 'image/gif':
+                    $img = imagecreatefromgif($file);
+                    $outfunc = 'imagegif';
+                    break;
+            }
+
+            $origwidth = imagesx( $img );
+            $origheight = imagesy( $img );
+
+            if(!is_null($h)){
+                $newheight = $h;
+            }else{
+                $newheight = $origheight;
+            }
+
+            if(!is_null($w)){
+                $newwidth = $w;
+            }else{
+                $newwidth = $origheight;
+            }
+
+            $cachePath = __DIR__ . "/../cache/{$newheight}x{$newwidth}/" . obje::relPath($basefile);
+
+            if(file_exists($cachePath)){
+                $file = $cachePath;
+            }else{
+                $tmpimg = imagecreatetruecolor($newwidth,$newheight);
+                imagecopyresampled($tmpimg,$img,0,0,0,0,$newwidth,$newheight,$origwidth,$origheight);
+
+                $dir = dirname($cachePath);
+                @umask(0022);
+                if(!is_dir($dir)){
+                    @mkdir($dir,0775,TRUE);
+                }
+                if(is_dir($dir)){
+                    $outfunc($tmpimg,$cachePath);
+                }
+
+                if(file_exists($cachePath)){
+                    $file = $cachePath;
+                }
+            }
+        }
+
+        header("Content-Description: File Transfer");
+        header("Content-type: $mime");
+        header("Content-Length: " . filesize($file));
+        if($attachment){
+            header("Content-disposition: attachment; filename=$attachmentName");
+        }
+
+        return readfile($file);
+    }
+
 
     function __call($func,$args){
         return call_user_func_array(Array($this->obje,$func),$args);
