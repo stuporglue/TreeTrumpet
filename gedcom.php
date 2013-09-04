89298f04ecec5ab40373b1cba4b3e3d4bded11ca
Michael Moore <stuporglue@gmail.com>
2013-09-04 09:21:20 -0500

    Removed old files, fixed gedcom and people redirect scripts

--- gedcom.php
+++ gedcom.php
@@ -1,10 +1,3 @@
-<?php 
-
-require_once(__DIR__ . '/lib/setup.php');
-
-header("Content-Type: application/gedcom");
-header("Content-Length: " .(string)(filesize('family.ged')) );
-header('Content-Disposition: attachment; filename="family.ged"');
-header("Content-Transfer-Encoding: binary\n");
-                 
-readfile('family.ged');
+<?php
+// Hand off to index.php
+require_once('index.php');
--- people.php
+++ people.php
@@ -1,24 +1,3 @@
-<?php require_once(__DIR__ . '/lib/setup.php'); ?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8"/>
-    </head>
-    <body>
-        <div class='tt-content'>
-            <div>
-                <h1>Table View</h1>
-                <?php require_once('lib/header.php'); ?>
-                </div>
-        </div>
-        <div id='tt-table'>
-
-        <?php 
-            require_once('lib/table_noscript.php');
-            print $noscript;
-        ?>
-
-        </div>
-        <?php require_once('lib/footer.php'); ?>
-    </body>
-</html>
+<?php
+// Hand off to index.php
+require_once('index.php');
